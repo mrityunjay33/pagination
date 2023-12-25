867d13993b1ff66770492fe5f89d96d6524d7be9
Mrityunjay Kumar <mk10399@gmail.com>
2023-12-26 00:23:20 +0530

    Pagination

--- src/App.js
+++ src/App.js
@@ -2,11 +2,67 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
+  const [values, setValues] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
 
+  const fetchData = async () => {
+    try {
+      const res = await fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json');
+      const data = await res.json();
+      console.log(data)
+      setValues(data);
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  }
+
+  useEffect(() =>{
+    fetchData();
+  },[]);
+
+  const pagination = (index) => {
+    setCurrentPage(index);
+  }
+  
+  const handleNext = () => {
+    const count = Math.ceil(values.length/itemsPerPage);
+    if(currentPage < count) setCurrentPage(prev => prev + 1);
+  }
+  const handlePrev = () => {
+    if(currentPage > 1) setCurrentPage(prev => prev - 1);
+  }
+
+  const lastIndex = currentPage * itemsPerPage;
+  const firstIndex = lastIndex - itemsPerPage;
+  const currentData = values.slice(firstIndex, lastIndex);
 
   return (
     <div>
-      Hello World
+      <h1>Emoloyee Data Table</h1>
+      <table style={{width:'100%'}}>
+        <tr style={{background:'#D6EEEE'}}>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+        </tr>
+        {currentData.map((value, idx)=><tr key={idx}>
+          <td>{value.id}</td>
+          <td>{value.name}</td>
+          <td>{value.email}</td>
+          <td>{value.role}</td>
+        </tr>)}
+      </table>
+      
+      <ul className='pagination'>
+      <button onClick={handlePrev}>Prev</button>
+        {Array.from({length: Math.ceil(values.length/itemsPerPage)}, (_, i) => 
+        <li onClick={() => pagination(i+1)} className={currentPage === i+1 ? 'active' : ''}>{i+1}</li>
+        )}
+      <button onClick={handleNext}>Next</button>
+      </ul>
     </div>
   );
 }
